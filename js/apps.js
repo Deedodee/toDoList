175305f81b3431881d37795340ba4d3d39340256
Dan <ddd11chevy@gmail.com>
2015-07-21 17:32:57 -0700

    Add all files

---
+++ js/apps.js
@@ -0,0 +1,15 @@
+var form = document.getElementById("toDoForm");
+var ul = document.getElementById("mainUl");
+var items = [];
+
+form.addEventListener("submit", function (evt) {
+var toDo = document.getElementById("value").value;
+items.push(toDo);
+evt.preventDefault();
+	ul.textContent = "";
+	for (var i = 0; i < items.length; i++){
+		var li = document.createElement("li");
+		li.textContent = items[i];
+		ul.appendChild(li);
+	}
+});
