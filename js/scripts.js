401db66a7ba642a82ae1fcc9814a4788bb7df891
Dan <ddd11chevy@gmail.com>
2015-07-23 14:40:05 -0700

    Deleted a pic and changed nav and added item mods

--- js/scripts.js
+++ js/scripts.js
@@ -11,6 +11,25 @@ form.addEventListener("submit", function (evt) {
 			var li = document.createElement("li");
 			li.textContent = items[i];
 			ul.appendChild(li);
+
+			var ul2 = document.createElement("ul");
+			ul.appendChild(ul2);
+
+			var li2 = document.createElement("li");
+			li2.setAttribute("id", "input");
+			ul2.appendChild(li2);
+
+			var input = document.createElement("input");
+			input.setAttribute("type", "checkbox");
+			li2.appendChild(input);
+
+			var edit = document.createElement("li");
+			edit.style.backgroundImage = "url(img/edit.png)";
+			ul2.appendChild(edit);
+
+			var vdelete = document.createElement("li");
+			vdelete.style.backgroundImage = "url(img/delete.png)";
+			ul2.appendChild(vdelete);
 		}
 	document.getElementById("value").value = "";
 });
