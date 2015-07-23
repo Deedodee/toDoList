0a04c0f381bfab937ca8cb861e9c39a7669f0074
Dan <ddd11chevy@gmail.com>
2015-07-23 16:33:28 -0700

    Stuff

--- js/scripts.js
+++ js/scripts.js
@@ -1,19 +1,33 @@
+// The To Do Form functionality
 var form = document.getElementById("toDoForm");
 var ul = document.getElementById("mainUl");
 var items = [];
-
 form.addEventListener("submit", function (evt) {
 	var toDo = document.getElementById("value").value;
 	items.push(toDo);
 	evt.preventDefault();
 		ul.textContent = "";
 		for (var i = 0; i < items.length; i++){
+			var div = document.createElement("div");
+			div.setAttribute("id", "container");
+			ul.appendChild(div);
+
 			var li = document.createElement("li");
+			li.setAttribute("class", "new");
 			li.textContent = items[i];
-			ul.appendChild(li);
+			div.appendChild(li);
 
 			var ul2 = document.createElement("ul");
-			ul.appendChild(ul2);
+			ul2.setAttribute("id", "ul2");
+			div.appendChild(ul2);
+
+			var vdelete = document.createElement("li");
+			vdelete.style.backgroundImage = "url(img/delete.png)";
+			ul2.appendChild(vdelete);
+
+			var edit = document.createElement("li");
+			edit.style.backgroundImage = "url(img/edit.png)";
+			ul2.appendChild(edit);
 
 			var li2 = document.createElement("li");
 			li2.setAttribute("id", "input");
@@ -21,15 +35,8 @@ form.addEventListener("submit", function (evt) {
 
 			var input = document.createElement("input");
 			input.setAttribute("type", "checkbox");
+			input.setAttribute("id", "check");
 			li2.appendChild(input);
-
-			var edit = document.createElement("li");
-			edit.style.backgroundImage = "url(img/edit.png)";
-			ul2.appendChild(edit);
-
-			var vdelete = document.createElement("li");
-			vdelete.style.backgroundImage = "url(img/delete.png)";
-			ul2.appendChild(vdelete);
 		}
 	document.getElementById("value").value = "";
 });
